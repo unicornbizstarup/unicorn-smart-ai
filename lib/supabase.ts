afafdce583bac662a6b85eb5ee645072a3275960
Den Master Fa - capvision@gmail.com <dencapvision@gmail.com>
2026-02-27 21:05:03 +0700

    ดำเนินการย้าย Server ไปยังสิงคโปร์ (Singapore)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://joryufdkqxlnaseocolf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvcnl1ZmRrcXhsbmFzZW9jb2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMzE3MTEsImV4cCI6MjA4NzcwNzcxMX0.QFg75UYcnm_aRiApY6d2zeXYdDF3YE41i28i0KHzG7M';
+const supabaseUrl = 'https://mewjhcheciafyuxkngqn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ld2poY2hlY2lhZnl1eGtuZ3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxOTMwMDUsImV4cCI6MjA4Nzc2OTAwNX0.oRQTlk-bpVNNxkEmklgaNZvLxIIvXJAJTZVyU-FK0bQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
